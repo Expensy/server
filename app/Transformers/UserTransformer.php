c7091e57bed766b7d53238d603a6ebeefb4ffbb0
Pierre Baron <pierre.r.baron22@gmail.com>
2016-03-30 22:09:11 +0100

    Added the projects in the JSON for GET user

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -4,8 +4,14 @@
 namespace App\Transformers;
 
 
+use Underscore\Types\Arrays;
+
 class UserTransformer extends Transformer
 {
+  public function __construct()
+  {
+  }
+
   public function basicTransform($item)
   {
     return [
@@ -17,11 +23,25 @@ class UserTransformer extends Transformer
 
   public function extendedTransform($item)
   {
-    return $this->basicTransform($item);
+    return Arrays::merge(
+        $this->basicTransform($item),
+        [
+            'projects' => $this->_getProjects($item)
+        ]);
   }
 
   public function fullTransform($item)
   {
     return $this->extendedTransform($item);
+  }
+
+  private function _getProjects($item)
+  {
+    return Arrays::each($item->projects->all(), function ($project) {
+      return [
+          'id'    => $project['id'],
+          'title' => $project['title']
+      ];
+    });
   }
 }
