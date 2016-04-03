c53f7cc60bc37e2ab8d915f416c0899e891a28f0
Pierre Baron <prbaron@users.noreply.github.com>
2016-04-03 19:43:26 +0100

    Update ProjectsController.php

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -65,7 +65,7 @@ class ProjectsController extends ApiController
 
     $createdProject = $this->projectRepository->create($inputs);
     $createdCategory = $this->categoryRepository->create([
-        'title'      => env('DEFAULT_CATEGORY_TITLE', 'category 1'),
+        'title'      => env('DEFAULT_CATEGORY_TITLE', 'Category 1'),
         'color'      => env('DEFAULT_CATEGORY_COLOR', '#419fdb'),
         'by_default' => true,
         'project_id' => $createdProject->id
