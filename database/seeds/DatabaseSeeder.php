dbffcd7cce7781ab8309b42525596e7e777c89bd
prbaron <prbaron22@gmail.com>
2016-05-10 22:50:15 +0100

    Fixed config files

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,23 +14,23 @@ class DatabaseSeeder extends Seeder
   public function run()
   {
     // $this->call(UserTableSeeder::class);
-
-    Model::unguard();
-
-    DB::table('users')->delete();
-
-    $users = [
-        ['name' => 'Ryan Chenkie', 'email' => 'ryanchenkie@gmail.com', 'password' => Hash::make('secret')],
-        ['name' => 'Chris Sevilleja', 'email' => 'chris@scotch.io', 'password' => Hash::make('secret')],
-        ['name' => 'Holly Lloyd', 'email' => 'holly@scotch.io', 'password' => Hash::make('secret')],
-        ['name' => 'Adnan Kukic', 'email' => 'adnan@scotch.io', 'password' => Hash::make('secret')],
-    ];
-
-    // Loop through each user above and create the record for them in the database
-    foreach ($users as $user) {
-      User::create($user);
-    }
-
-    Model::reguard();
+//
+//    Model::unguard();
+//
+//    DB::table('users')->delete();
+//
+//    $users = [
+//        ['name' => 'Ryan Chenkie', 'email' => 'ryanchenkie@gmail.com', 'password' => Hash::make('secret')],
+//        ['name' => 'Chris Sevilleja', 'email' => 'chris@scotch.io', 'password' => Hash::make('secret')],
+//        ['name' => 'Holly Lloyd', 'email' => 'holly@scotch.io', 'password' => Hash::make('secret')],
+//        ['name' => 'Adnan Kukic', 'email' => 'adnan@scotch.io', 'password' => Hash::make('secret')],
+//    ];
+//
+//    // Loop through each user above and create the record for them in the database
+//    foreach ($users as $user) {
+//      User::create($user);
+//    }
+//
+//    Model::reguard();
   }
 }
