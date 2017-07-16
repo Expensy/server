93c5e6c3959ac42b11646787d18647c9f88da50f
Pierre Baron <pierre.r.baron22@gmail.com>
2017-07-16 14:23:18 +0100

    Fixed r

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
    * @return void
    */
   public function boot() {
-    //
+    Schema::defaultStringLength(191);
   }
 
   /**
